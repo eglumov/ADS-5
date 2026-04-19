f522ed90d76711cb54298243cd596fd856b6739b
eglumov <eugen.glumov@yandex.ru>
2026-04-19 17:09:02 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,38 +2,39 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
-private:
-    T data[size];
-    int topIndex;
+ public:
+  TStack() : topIndex(-1) {}
 
-public:
-    TStack() : topIndex(-1) {}
+  bool isEmpty() const {
+    return topIndex == -1;
+  }
 
-    bool isEmpty() const {
-        return topIndex == -1;
-    }
+  bool isFull() const {
+    return topIndex == size - 1;
+  }
 
-    bool isFull() const {
-        return topIndex == size - 1;
-    }
+  void push(const T& value) {
+    data[++topIndex] = value;
+  }
 
-    void push(const T& value) {
-        data[++topIndex] = value;
-    }
+  void pop() {
+    --topIndex;
+  }
 
-    void pop() {
-        --topIndex;
-    }
+  T& top() {
+    return data[topIndex];
+  }
 
-    T& top() {
-        return data[topIndex];
-    }
+  const T& top() const {
+    return data[topIndex];
+  }
 
-    const T& top() const {
-        return data[topIndex];
-    }
+ private:
+  static const int kSize = size;
+  T data[kSize];
+  int topIndex;
 };
 
 #endif // INCLUDE_TSTACK_H_
